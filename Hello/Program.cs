ca5caacf01fd3276a4c16b1b5488e8ecccf1b5ce
kanet <kanet@NANDINI-007>
2023-02-11 20:41:06 +0530

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD");
+            Console.Write("Learning C#\n");
+            Console.Write("New for it");
+            Console.ReadLine();
+
+        }
+    }
+}
